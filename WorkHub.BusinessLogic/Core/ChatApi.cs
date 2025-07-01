606c821672fb07d2693c7c6202bcc23c0d1ef2c7
saniok777 <alexandru.stoica@isa.utm.md>
2025-07-01 11:17:56 +0300

    Fix. saniok

---
+++ WorkHub.BusinessLogic/Core/ChatApi.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkHub.BusinessLogic.DBModel;
+using WorkHub.Domain.Enums;
+using WorkHub.Web.Models.Chat;
+
+namespace WorkHub.BusinessLogic.Core
+{
+     public class ChatApi
+     {
+          public void CreateChatAction(string title, string createdBy, string topic)
+          {
+               using (var db = new UserContext())
+               {
+                    var chat = new Chat
+                    {
+                         Title = title,
+                         CreatedBy = createdBy,
+                         Topic = topic
+                    };
+
+                    db.ChatRooms.Add(chat);
+                    db.SaveChanges();
+               }
+          }
+
+     }
+
+}
