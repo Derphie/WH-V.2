c30f878fbcd9c85b0d8ccd95f3d3d0760ae01c43
RacuSandu <sandu.racu2004@gmail.com>
2025-07-01 11:47:03 +0300

    Another changes

--- WorkHub.BusinessLogic/Core/AdminApi.cs
+++ WorkHub.BusinessLogic/Core/AdminApi.cs
@@ -9,5 +9,49 @@ namespace WorkHub.BusinessLogic.Core
 {
      public class AdminApi
      {
+          public List<UDbTable> GetAllUsersAction()
+          {
+               using (var db = new UserContext())
+               {
+                    return db.Users.ToList();
+               }
+          }
+
+          public UDbTable GetUserByIdAction(int id)
+          {
+               using (var db = new UserContext())
+               {
+                    return db.Users.FirstOrDefault(u => u.Id == id);
+               }
+          }
+
+          public bool UpdateUserAction(UDbTable user)
+          {
+               using (var db = new UserContext())
+               {
+                    var existingUser = db.Users.Find(user.Id);
+                    if (existingUser == null) return false;
+
+                    existingUser.Username = user.Username;
+                    existingUser.Email = user.Email;
+                    existingUser.Level = user.Level;
+
+                    db.SaveChanges();
+                    return true;
+               }
+          }
+
+          public bool DeleteUserAction(int id)
+          {
+               using (var db = new UserContext())
+               {
+                    var user = db.Users.Find(id);
+                    if (user == null) return false;
+
+                    db.Users.Remove(user);
+                    db.SaveChanges();
+                    return true;
+               }
+          }
      }
 }
